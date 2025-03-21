fe94fb7e58f62c06c0b7e550c79d10a97e762fa5
choudharymahipal <mschoudhary81@gmail.com>
2025-03-21 22:12:12 +0530

    add view regitraion page

--- src/app/Components/view-registration/view-registration.component.ts
+++ src/app/Components/view-registration/view-registration.component.ts
@@ -1,24 +1,45 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../Shared/Services/auth.service';
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-view-registration',
   templateUrl: './view-registration.component.html',
-  styleUrl: './view-registration.component.scss'
+  styleUrl: './view-registration.component.scss',
 })
 export class ViewRegistrationComponent {
-  data:any;
+  data: any;
+  selectedDataIndex: number=-1;
 
-  constructor(private authService:AuthService) { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.getAllRegistrations();
   }
 
-  getAllRegistrations(){
+  getAllRegistrations() {
     // Call the service to get all registrations
-    this.authService.getAllRegistrations().subscribe((data)=>{
+    this.authService.getAllRegistrations().subscribe((data) => {
       this.data = data;
     });
   }
+
+  openModal(data: any) {
+    // Open the modal
+    this.selectedDataIndex = data.id;
+    const modalElement = document.getElementById('modal-default');
+    if (modalElement) {
+      const myModal = new Modal(modalElement);
+      myModal.show();
+    } else {
+      console.error('Modal element not found');
+    }
+  }
+
+  // Function to close the modal
+  closeModal() {
+    this.selectedDataIndex = -1;
+  }
+
+  bindDataTable() {}
 }
